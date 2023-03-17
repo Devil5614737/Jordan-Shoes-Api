90e3cbf2f1930a3487edf92b448832475f544196
Devil5614737 <kaushiksheel9@gmail.com>
2023-03-17 10:38:15 +0530

    added find by slug feature

--- index.js
+++ index.js
@@ -11,9 +11,20 @@ app.get("/shoes", (req, res) => {
   return res.status(200).json(data);
 });
 
-app.get("/shoes/:id", (req, res) => {
-  const shoeId = req.params.id.toString();
-  const shoe = data.find((shoe) => shoe.id.toString() === shoeId);
+// app.get("/shoes/:id", (req, res) => {
+//   const shoeId = req.params.id.toString();
+//   const shoe = data.find((shoe) => shoe.id.toString() === shoeId);
+// if (shoe) {
+//   return res.status(200).json(shoe);
+// } else {
+//   return res.status(400).json("not found");
+// }
+// });
+
+// get shoe by name
+app.get("/shoes/:slug", (req, res) => {
+  const shoeName = req.params.slug;
+  const shoe = data.find((shoe) => shoe.slug === shoeName);
   if (shoe) {
     return res.status(200).json(shoe);
   } else {
