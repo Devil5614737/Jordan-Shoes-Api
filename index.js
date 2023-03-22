43a05cf614f93cda3779c3b31167c851f90017f3
Devil5614737 <kaushiksheel9@gmail.com>
2023-03-22 16:10:16 +0530

    .

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 const express = require("express");
 const app = express();
 const data = require("./data");
-const cors=require("cors")
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 app.get("/shoes", (req, res) => {
   return res.status(200).json(data);
 });
-
-// app.get("/shoes/:id", (req, res) => {
-//   const shoeId = req.params.id.toString();
-//   const shoe = data.find((shoe) => shoe.id.toString() === shoeId);
-// if (shoe) {
-//   return res.status(200).json(shoe);
-// } else {
-//   return res.status(400).json("not found");
-// }
-// });
 
 // get shoe by name
 app.get("/shoes/:slug", (req, res) => {
